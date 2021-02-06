66c67567742eff184b39d69dcc256d2f8827cc1a
Andreas D <andreas.ds90@gmail.com>
2021-02-06 23:44:06 +0700

    #1 update database query return result and failed status
    - result: last inserted id, affected row, select query
    - failed: failed response

--- online-store/online_store/helper/database.py
+++ online-store/online_store/helper/database.py
@@ -1,6 +1,8 @@
 import mysql.connector as mysql
 
+from http import HTTPStatus
 from mysql.connector import Error
+from online_store.helper.response import failed_response
 from online_store.helper.singleton import Singleton
 
 class Database(metaclass=Singleton):
@@ -32,10 +34,19 @@ class Database(metaclass=Singleton):
 
         Raises:
             SystemError: error when database is not configured yet
+
+        Returns:
+            (tuple):
+                - (dict): query result, lastrowid and rowcount
+                - (flask.Response): failed response
         """
         if not self.__ready:
             raise SystemError('Database is not configured yet.')
 
+        failed = None
+        db = None
+        cursor = None
+        result = None
         try:
             # connect to database
             db = mysql.connect(
@@ -49,15 +60,35 @@ class Database(metaclass=Singleton):
             if db.is_connected():
                 cursor = db.cursor()
                 cursor.execute(query, values)
+
+                # get insert id or affected row
+                result = {
+                    'lastrowid': cursor.lastrowid,
+                    'rowcount': cursor.rowcount,
+                }
+
                 db.commit()
             else:
                 print('Failed connect to database.')
+                failed = failed_response(
+                    HTTPStatus.INTERNAL_SERVER_ERROR,
+                    'ERROR failed connect to database',
+                )
         except Error as e:
             print('Error connection to database:', e)
+            failed = failed_response(
+                HTTPStatus.INTERNAL_SERVER_ERROR,
+                ' '.join([ 'ERROR connection to database.', str(e) ]),
+            )
         finally:
-            if db.is_connected():
-                cursor.close()
+            if db is not None and \
+                db.is_connected():
+                if cursor is not None:
+                    cursor.close()
+
                 db.close()
+
+        return result, failed
 
     def fetchQuery(self, query, values):
         """ Fetch query result
@@ -70,11 +101,17 @@ class Database(metaclass=Singleton):
             SystemError: error when database is not configured yet
 
         Returns:
-            array: query result
+            (tuple):
+                - (array): query result
+                - (flask.Response): failed response
         """
         if not self.__ready:
             raise SystemError('Database is not configured yet.')
 
+        failed = None
+        db = None
+        cursor = None
+        result = None
         try:
             # connect to database
             db = mysql.connect(
@@ -91,11 +128,22 @@ class Database(metaclass=Singleton):
                 result = cursor.fetchall()
             else:
                 print('Failed connect to database.')
+                failed = failed_response(
+                    HTTPStatus.INTERNAL_SERVER_ERROR,
+                    'ERROR failed connect to database',
+                )
         except Error as e:
             print('Error connection to database:', e)
+            failed = failed_response(
+                HTTPStatus.INTERNAL_SERVER_ERROR,
+                ' '.join([ 'ERROR connection to database.', str(e) ]),
+            )
         finally:
-            if db.is_connected():
-                cursor.close()
+            if db is not None and \
+                db.is_connected():
+                if cursor is not None:
+                    cursor.close()
+
                 db.close()
 
-        return result
+        return result, failed
